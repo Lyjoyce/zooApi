d4d76f5d62e377dcdd11a4bd251afdf65c2ca724
lyjoice <lahely.na@gmail.com>
2025-07-01 17:19:35 +0200

    pb variables d'env

--- src/main/java/com/zoo/api/ZooApiApplication.java
+++ src/main/java/com/zoo/api/ZooApiApplication.java
@@ -3,25 +3,15 @@ package com.zoo.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import com.zoo.api.ZooApiApplication;
 
 @SpringBootApplication
 public class ZooApiApplication {
 
-    public static void main(String[] args) {
-        // Charge le fichier .env (par défaut à la racine du projet)
-        Dotenv dotenv = Dotenv.load();
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(ZooApiApplication.class);
 
-        // Définit les propriétés système à partir des variables d'environnement du .env
-        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("ADMIN_USER", dotenv.get("ADMIN_USER"));
-        System.setProperty("ADMIN_PASSWORD", dotenv.get("ADMIN_PASSWORD"));
-
-        // Lancement de l'application Spring Boot
-        SpringApplication.run(ZooApiApplication.class, args);
-    }
+		app.setAdditionalProfiles("dev");
+		app.run(args);
+	}
 }
