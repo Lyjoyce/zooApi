7c5a95097078730aaf6bbc3f6e2bc8bc99123c2f
lyjoice <lahely.na@gmail.com>
2025-07-21 16:38:52 +0200

    add route api/v1/reservationTicket

--- src/main/java/com/zoo/api/security/SecurityConfig.java
+++ src/main/java/com/zoo/api/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
             	    .requestMatchers(HttpMethod.OPTIONS, "/api/v1/auth/login").permitAll()
             	    .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
             	    .requestMatchers("/api/v1/employees/**").hasAnyRole("EMPLOYEE", "VETERINAIRE")
-            	    //.requestMatchers("/api/v1/adult/**").permitAll()  // supprimé pour respecter la règle
+            	    .requestMatchers("/api/v1/reservationTicket").permitAll() // autorise sans authentification
             	    .requestMatchers("/api/v1/avis/**").permitAll()
             	    .anyRequest().authenticated()
             	)
