b742c1dc0a345a4941dfffdcc278309469ea408d
lyjoice <lahely.na@gmail.com>
2025-07-21 17:54:17 +0200

    test pour voir secur config

--- src/main/java/com/zoo/api/security/SecurityConfig.java
+++ src/main/java/com/zoo/api/security/SecurityConfig.java
@@ -29,58 +29,10 @@ public class SecurityConfig {
     private final CustomUserDetailsService userDetailsService;
     
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*")); // autoriser toutes les origines pour test curl
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-       // config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-            .authorizeHttpRequests(auth -> auth
-            	    .requestMatchers("/api/v1/auth/**", "/api/v1/employee/login").permitAll()
-            	    .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
-            	    .requestMatchers("/api/v1/employees/**").hasAnyRole("EMPLOYEE", "VETERINAIRE")
-            	    // autorise sans authentification
-            	    .requestMatchers(HttpMethod.POST, "/api/v1/reservationTicket").permitAll()
-            	    .requestMatchers("/api/v1/avis/**").permitAll()
-            	    .anyRequest().authenticated()
-            	)
-
-                    
-                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider());
-             // .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
-
-            return http.build();
-        }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
     }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
 }
