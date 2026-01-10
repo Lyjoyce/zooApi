09d9631e67cd31ffead31981a869f863fce8454c
lyjoice <lahely.na@gmail.com>
2026-01-10 21:04:31 +0100

    Adult.java : @Builder.Default

--- src/main/java/com/zoo/api/entities/Adult.java
+++ src/main/java/com/zoo/api/entities/Adult.java
@@ -2,23 +2,9 @@ package com.zoo.api.entities;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import com.zoo.api.enums.AdultType;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "adult")
@@ -27,19 +13,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Adult {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String firstName;
     private String lastName;
-    
+
     @Column(nullable = false, unique = true)
     private String email;
 
     @Enumerated(EnumType.STRING)
     private AdultType type;
-    
+
     @OneToMany(mappedBy = "adult", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
     private List<Ticket> tickets = new ArrayList<>();
 }
